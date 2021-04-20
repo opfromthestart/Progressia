5688144e8046dd0a9211cb2e1e3fbf1cce6eb941
opfromthestart <opfromthestart@gmail.com>
2021-04-20 08:46:01 -0400

    Fixed double/midair jumps
    But only temporarily

--- src/main/java/ru/windcorp/progressia/test/TestPlayerControls.java
+++ src/main/java/ru/windcorp/progressia/test/TestPlayerControls.java
@@ -276,7 +276,7 @@ public class TestPlayerControls {
 
 	private void jump() {
 		if (ClientState.getInstance() == null || !ClientState.getInstance().isReady()
-				|| (Math.abs(getEntity().getVelocity().z) != 0)) {// TODO actual
+				|| (Math.abs(getEntity().getVelocity().z) > 0.1)) {// TODO actual
 																	// isOnGround
 																	// method
 			return;
